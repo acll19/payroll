ea4e0afc532232571b1dc4c7565dcc8327daa914
Armando Collazo <acollazo@thoughtworks.com>
2017-11-07 00:32:35 -0500

    EmployeeService to use employee target from config

--- src/main/java/domain/services/EmployeeService.java
+++ src/main/java/domain/services/EmployeeService.java
@@ -1,11 +1,36 @@
 package domain.services;
 
 import domain.Employee;
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
 import java.util.List;
 
+@RequestScoped
 public class EmployeeService {
+
+    private WebTarget target;
+
+    @Inject
+    @ConfigurationValue("employees.service.target")
+    String employeesTarget;
+
+    @PostConstruct
+    public void init() {
+        Client client = ClientBuilder.newBuilder().build();
+        this.target = client.target(employeesTarget);
+    }
+
     public List<Employee> fetchEmpoyees() {
-        return null;
+        target.path("/employees");
+        Employee[] employees = target.request().accept(MediaType.APPLICATION_JSON).get(Employee[].class);
+        return Arrays.asList(employees);
     }
 }
